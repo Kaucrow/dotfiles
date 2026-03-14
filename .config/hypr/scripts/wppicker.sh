00c07644291c4e12cea66b508e48a71e11e94248
Kaucrow <kaucrow@protonmail.com>
2026-03-14 00:08:53 -0400

    feat: Updated dotfiles

--- .config/hypr/scripts/wppicker.sh
+++ .config/hypr/scripts/wppicker.sh
@@ -11,11 +11,12 @@ cd "$WALLPAPER_DIR" || exit 1
 IFS=$'\n'
 
 # === ICON-PREVIEW SELECTION WITH ROFI, SORTED BY NEWEST ===
-SELECTED_WALL=$(for a in $(ls -t *.jpg *.png *.gif *.jpeg 2>/dev/null); do echo -en "$a\0icon\x1f$a\n"; done | rofi -dmenu -p "")
+SELECTED_WALL=$(for a in $(ls -t *.jpg *.png *.gif *.jpeg 2>/dev/null); do echo -en "$a\0icon\x1f$a\n"; done | ~/.config/rofi/launcher/dmenulauncher.sh)
 [ -z "$SELECTED_WALL" ] && exit 1
 SELECTED_PATH="$WALLPAPER_DIR/$SELECTED_WALL"
 
 # === SET WALLPAPER ===
+#matugen image "$SELECTED_PATH"
 kitty --class="matugen-picker" -e matugen -m dark -t scheme-content image "$SELECTED_PATH"
 sleep 0.5
 
